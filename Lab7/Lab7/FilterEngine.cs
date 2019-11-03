6376105cd3f25045a4fe368a2d3d72eeb43ac8df
henry-likellion <33973504+henry-likellion@users.noreply.github.com>
2019-11-04 01:08:56 +1100

    Lab 7 attempt 1

---
+++ Lab7/Lab7/FilterEngine.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7
+{
+    public static class FilterEngine
+    {
+        public static List<Frame> FilterFrames (List<Frame> frames, EFeatures features)
+        {
+            List<Frame> filteredFrames = new List<Frame>();
+
+            for (int i = 0; i < frames.Count; ++i)
+            {
+                Console.WriteLine(frames[i].Features);
+                if ((frames[i].Features & features) != 0)
+                {
+                    filteredFrames.Add(frames[i]);
+                }
+            }
+            return filteredFrames;
+        }
+
+        public static List<Frame> FilterOutFrames(List<Frame> frames, EFeatures features)
+        {
+            List<Frame> filteredFrames = new List<Frame>();
+
+            for (int i = 0; i < frames.Count; ++i)
+            {
+                Console.WriteLine(frames[i].Features);
+                if ((frames[i].Features & features) == 0)
+                {
+                    filteredFrames.Add(frames[i]);
+                }
+            }
+            return filteredFrames;
+        }
+
+        public static List<Frame> Intersect(List<Frame> frames1, List<Frame> frames2)
+        {
+            List<Frame> intersectionFrames = new List<Frame>();
+            for (int i = 0; i < frames1.Count; ++i)
+            {
+                for (int j = 0; j < frames2.Count; ++j)
+                {
+                    if (frames1[i].Name == frames2[j].Name && frames1[i].Features == frames2[j].Features)
+                    {
+                        intersectionFrames.Add(frames1[i]);
+                    }
+                }
+            }
+
+            return intersectionFrames;
+        }
+
+        public static List<int> GetSortKeys(List<Frame> frames, List<EFeatures> features)
+        {
+            List<int> framesKeys = new List<int>(frames.Count);
+
+            for (int i = 0; i < frames.Count; ++i)
+            {
+                framesKeys.Add(1);
+
+                for (int j = 0; j < features.Count; ++j)
+                {
+                    if ((frames[i].Features & features[j]) == features[j])
+                    {
+                        framesKeys[i] = 128 - j;
+                        break;
+                    }
+                }
+            }
+
+            return framesKeys;
+        }
+    }
+}
---
+++ Lab7/Lab7/Frame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7
+{
+    public class Frame
+    {
+        public EFeatures Features { get; private set; }
+        public uint ID { get; private set; }
+        public string Name { get; private set; }
+
+        public Frame (uint id, string name)
+        {
+            ID = id;
+            Name = name;
+            Features = EFeatures.Default;
+        }
+
+        public void ToggleFeatures (EFeatures features)
+        {
+            Features ^= features;
+        } 
+
+        public void TurnOnFeatures (EFeatures features)
+        {
+            Features |= features;
+        }
+
+        public void TurnOffFeatures (EFeatures features)
+        {
+            Features &= ~features;
+        }
+    }
+}
